4d892e71790fd16a09ed31627cd2a98ac5ee8c26
Yashas Surendra <96817989+kjkOver9k@users.noreply.github.com>
2023-01-11 21:51:38 +0530

    Update atca.py

--- atca.py
+++ atca.py
@@ -2,29 +2,54 @@ import threading
 from time import sleep
 from random import choice
 
-semaphore = threading.Semaphore(1)
+runway = threading.BoundedSemaphore(2)
+runways = {
+    "1": '',
+    "2": ''
+}
 
-def weather_condition():
-    return choice([True, False])
+weather = choice([True, False])
+
 
 def plane(plane_no):
-    print(f'Plane {plane_no} is waiting\n')
-    while semaphore._value <= 0 and weather_condition():
-        pass
-    semaphore.acquire()
-    print(f"Plane {plane_no} has acquired runway\n")
-    print(f'The plane {plane_no} is landing\n')
-    sleep(2)
-    semaphore.release()
-    print(f"""Plane {plane_no} has finished landing, the runway is free\n""")
+    global weather
+    if weather:
+        if runways["1"] != '' and runways["2"] != '':
+            print(f'Plane {plane_no} is waiting for both the runway\n')
+        elif runways["1"] == '' and runways["2"] != '':
+            print(f'Plane {plane_no} is waiting for the runway 1\n')
+        elif runways["2"] == '' and runways["1"] != '':
+            print(f'Plane {plane_no} is waiting for the runway 2\n')
+        else:
+            print(f'Plane {plane_no} is going into runway 1 since both the runways are free\n')
+        runway.acquire()
+        if runways["1"] == '' and runways["2"] == '':
+            runways["1"] = plane_no
+        elif runways["1"] == '' and runways["2"] != '':
+            runways["1"] = plane_no
+        elif runways["2"] == '' and runways["1"] != '':
+            runways["2"] = plane_no
+        a = list(runways.values()).index(plane_no)+1
+        print(f"Plane {plane_no} has acquired the runway {a} and needs 10s seconds to land\n")
+        weather = choice([True, False])
+        sleep(5)
+        print(f"""Plane {plane_no} has finished landing in runway {a} \n""")
+        print(f"The runway {a} is free\n")
+        runways[str(a)] = ''
+        runway.release()
+    else:
+        print(f"The weather is terrible for plane {plane_no}")
+        weather = choice([True, False])
+
+
 
 threads = []
 
 for i in range(3):
     t = threading.Thread(target=plane, args=[i+1])
     t.start()
+    sleep(1)
     threads.append(t)
 
 for thread in threads:
     thread.join()
-
